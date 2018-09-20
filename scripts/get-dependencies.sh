bbe0dda46677f363d7d7dc294203fd54a1b65be9
johnerikhalse <johnerikhalse@gmail.com>
2018-09-20 16:19:54 +0200

    Updated dependency versions New veidemann-api version

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-API_VERSION=0.1.4
+API_VERSION=0.1.10
 
 rm -rf protobuf
 mkdir protobuf
 cd protobuf
 
 # Download protoc
-wget -q https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
-unzip protoc-3.5.1-linux-x86_64.zip
-rm protoc-3.5.1-linux-x86_64.zip
+wget -q https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
+unzip protoc-3.6.1-linux-x86_64.zip
+rm protoc-3.6.1-linux-x86_64.zip
 
 # Download Veidemann API
 wget -O - -q https://github.com/nlnwa/veidemann-api/archive/${API_VERSION}.tar.gz | tar --strip-components=2 -zx
